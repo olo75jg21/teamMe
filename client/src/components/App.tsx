fff55c0589948e9b2940ff6fd79a4e685c4fa61a
olo75jg21 <olo75jg12@gmail.com>
2022-07-16 12:25:28 +0200

    configued tailwind

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import '../index.css';
 
 export const App = (): JSX.Element => {
   return (
-    <div>App</div>
+    <div>
+      <h1 className='text-3xl font-bold underline text-blue-600'>helolololo</h1>
+    </div>
   );
 };
