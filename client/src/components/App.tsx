75ffc485197cf1cc5ace284a8696f08e13676a4c
olo75jg21 <olo75jg12@gmail.com>
2022-07-23 14:53:25 +0200

    setup proxy

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../index.css';
 
+import Register from './Register';
+
 export const App = (): JSX.Element => {
   return (
     <div>
       <h1 className='text-3xl font-bold underline text-blue-600'>helolololo</h1>
+      <Register />
     </div>
   );
 };
---
+++ client/src/components/Register.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from 'axios';
+
+type FormValues = {
+  email: string;
+  username: string;
+  password: string;
+};
+
+export default function Register() {
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = data => {
+    axios.post('/users/register', data)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="email" {...register("email")} />
+      <input {...register("username")} />
+      <input type="password" {...register("password")} />
+
+      <input type="submit" />
+    </form>
+  );
+}
