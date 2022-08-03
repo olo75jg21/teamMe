3eb8ab3024e45d713a205f2ea78425681fab0d27
olo75jg21 <olo75jg12@gmail.com>
2022-08-03 17:00:42 +0200

    configured redux

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../redux/hooks';
+import { setCredentials } from '../redux/features/authSlice';
+
 const LandingPage = (): JSX.Element => {
+  const user = useAppSelector((state) => state.auth.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setCredentials({ user: ' 12' }));
+    console.log(user);
+  });
+
   return (
     <div>Landing Page</div>
   );
