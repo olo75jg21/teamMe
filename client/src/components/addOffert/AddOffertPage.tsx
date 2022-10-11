2a3cbb0a55599ef0c507181437ed5374ecf6705b
Jakub Olcha <olo75jg12@gmail.com>
2022-10-11 20:10:58 +0200

    add scaffolding for add offert form

--- client/src/components/addOffert/AddOffertPage.tsx
+++ client/src/components/addOffert/AddOffertPage.tsx
@@ -1,7 +1,35 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type formData = {
+  game: string;
+  rank: string;
+  description: string;
+};
+
 const AddOffertPage = (): JSX.Element => {
+  const { register, handleSubmit, watch, formState: { errors }} = useForm<formData>();
+
+  const onSubmit: SubmitHandler<formData> = data => {
+    console.log('eo')
+  };
+
   return (
     <div>
-      add offert page
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Game</label>
+        <input {...register('game')}></input>
+
+        <label>Rank</label>
+        <input {...register('rank')}></input>
+
+        <label>Description</label>
+        <input {...register('description')}></input>
+        <input 
+          type='submit' 
+          value='Add'
+          className='bg-blue-500 cursor-pointer hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button'
+        />
+      </form>
     </div>
   )
 };
