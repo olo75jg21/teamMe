16150195d202420bb4db3e7d741de6d193ad4ba1
olo75jg21 <olo75jg12@gmail.com>
2022-11-20 23:07:08 +0100

    comments

--- client/src/components/landing/landingUtils.ts
+++ client/src/components/landing/landingUtils.ts
@@ -1,11 +1,15 @@
-const calculateTimePassed = (createdAt: string) => {
+/**
+ * Utils functions to calculate passed days since added offer
+ */
+
+const calculateTimePassed = (createdAt: string): number => {
     const differenceInTime = new Date().getTime() - new Date(createdAt).getTime();
     const differenceInDays = differenceInTime / (1000 * 3600 * 24);
 
     return Math.ceil(differenceInDays);
 };
 
-export const renderPassedDays = (createdAt: string) => {
+export const renderPassedDays = (createdAt: string): string => {
     if (calculateTimePassed(createdAt) === 0) {
         return 'Today';
     }
