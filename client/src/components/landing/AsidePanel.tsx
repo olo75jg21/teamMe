2c95e4a122f0f6a09541f946dc8bb7070d5fa164
Jakub Olcha <olo75jg12@gmail.com>
2023-04-27 17:20:48 +0200

    fix problem with height

--- client/src/components/landing/AsidePanel.tsx
+++ client/src/components/landing/AsidePanel.tsx
@@ -32,47 +32,54 @@ export const AsidePanel = ({ onFilterChange }: Props): JSX.Element => {
   };
 
   return (
-    <div className="bg-gray-100 rounded-md p-4 mb-4">
+    <div className="bg-gray-100 rounded-md p-4 m-5">
       <div className="flex flex-col mb-2">
-        <label htmlFor="tile" className="text-sm font-medium mb-1">Title:</label>
-        <input type="text" id="title" name="tile" value={title} onChange={handleTitleChange} className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:border-blue-400" />
+        <label htmlFor="title" className="text-sm font-medium mb-1">Title:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          onChange={handleTitleChange}
+          className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:border-blue-400"
+        />
       </div>
       <div className="flex flex-col mb-2">
         <label htmlFor="age_min" className="text-sm font-medium mb-1">Age Min:</label>
-        <input type="number" id="age_min" name="age_min" value={ageMin} onChange={handleAgeMinChange} className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:border-blue-400" />
+        <input
+          type="number"
+          id="age_min"
+          name="age_min"
+          value={ageMin}
+          onChange={handleAgeMinChange}
+          className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:border-blue-400"
+        />
       </div>
       <div className="flex flex-col mb-2">
         <label htmlFor="age_max" className="text-sm font-medium mb-1">Age Max:</label>
-        <input type="number" id="age_max" name="age_max" value={ageMax} onChange={handleAgeMaxChange} className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:border-blue-400" />
+        <input
+          type="number"
+          id="age_max"
+          name="age_max"
+          value={ageMax}
+          onChange={handleAgeMaxChange}
+          className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:border-blue-400"
+        />
       </div>
       <div className="flex flex-col mb-2">
         <label htmlFor="game" className="text-sm font-medium mb-1">Game:</label>
-        <select id="game" name="game" value={game} onChange={handleGameChange} className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:border-blue-400">
+        <select
+          id="game"
+          name="game"
+          value={game}
+          onChange={handleGameChange}
+          className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring focus:border-blue-400"
+        >
           <option value="">Select Game</option>
           <option value="lol">League of Legends</option>
           <option value="cs">Counter-Strike: Global Offensive</option>
           <option value="valorant">Valorant</option>
         </select>
-      </div>
-      <div>
-
-        {
-          game === 'lol' && <div>
-            LOL
-          </div>
-        }
-
-        {
-          game === 'cs' && <div>
-            LOL
-          </div>
-        }
-
-        {
-          game === 'valorant' && <div>
-            LOL
-          </div>
-        }
       </div>
     </div>
   );
