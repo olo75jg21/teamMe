abc806f1fc54d89d6648b6caaf2f75ff97e11e59
Jakub Olcha <olo75jg12@gmail.com>
2023-05-16 20:45:56 +0200

    fix problem with add new offer button on landing page

--- client/src/components/landing/AddNewOfferButton.tsx
+++ client/src/components/landing/AddNewOfferButton.tsx
@@ -2,11 +2,9 @@ import { NavLink } from 'react-router-dom';
 
 const AddNewOfferButton = (): JSX.Element => {
   return (
-    <div
+    <NavLink
       className='cursor-pointer text-gray-100 text-xl font-bold rounded rounded-md bg-violet-700 hover:bg-violet-800 mx-5 mt-6 mb-7 h-12 flex justify-center items-center'
-    >
-      <NavLink to={'/newoffer'}>Add new offer</NavLink>
-    </div>
+      to={'/newoffer'}>Add new offer</NavLink>
   );
 };
 
